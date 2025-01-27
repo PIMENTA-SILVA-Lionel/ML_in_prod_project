55984722ec0f635f110cf7cd5b5b927abcbbbcd9
Lionel PIMENTA SILVA <lionel.pimenta.silva@efrei.net>
2025-01-27 16:34:08 +0100

    creating e2e tests and yml

---
+++ e2e_test/e2e_test.js
@@ -0,0 +1,37 @@
+// Import des modules nécessaires
+const { Builder, By } = require('selenium-webdriver');
+const assert = require('assert');
+
+// Définition du test
+describe('e2e_test', function () {
+  this.timeout(30000); // Timeout étendu pour les tests E2E
+  let driver;
+
+  beforeEach(async function () {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+
+  afterEach(async function () {
+    await driver.quit();
+  });
+
+  it('Remplir le formulaire et envoyer', async function () {
+    console.log('Ouverture du site web...');
+    await driver.get('http://localhost:3000/');
+    console.log('Site web chargé.');
+    
+    console.log('Réglage de la taille de la fenêtre...');
+    await driver.manage().window().setRect({ width: 810, height: 816 });
+  
+    console.log('Remplissage des champs...');
+    await driver.findElement(By.name('sepalLength')).sendKeys('1');
+    await driver.findElement(By.name('sepalWidth')).sendKeys('1');
+    await driver.findElement(By.name('petalLength')).sendKeys('1');
+    await driver.findElement(By.name('petalWidth')).sendKeys('1');
+  
+    console.log('Soumission du formulaire...');
+    await driver.findElement(By.css('button')).click();
+  
+    console.log('Test terminé.');
+  });
+});
